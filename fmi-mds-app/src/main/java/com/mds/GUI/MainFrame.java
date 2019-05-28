f6f0523ed9cb1b8c7f864d05042a2e047a1ea6ae
kellasone <a_mihai_lo@yahoo.ro>
2019-05-28 12:18:27 +0300

    Added Tab pannel and test class to use the gui

--- fmi-mds-app/src/main/java/com/mds/GUI/MainFrame.java
+++ fmi-mds-app/src/main/java/com/mds/GUI/MainFrame.java
@@ -1,4 +1,18 @@
 package com.mds.GUI;
 
-public class MainFrame {
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+    private JTabbedPane tabbedPane1;
+
+    public MainFrame()
+    {
+        setTitle("Fmi APP");
+        setSize(750, 500);
+        Dimension minDimension = new Dimension(750, 500);
+        Dimension maxDimension = new Dimension(1800, 800);
+        setMinimumSize(minDimension);
+        setMaximumSize(maxDimension);
+    }
 }
---
+++ fmi-mds-app/src/main/java/com/mds/GUI/Test.java
@@ -0,0 +1,8 @@
+package com.mds.GUI;
+
+public class Test {
+    public static void main(String[] args) {
+        MainFrame x = new MainFrame();
+        x.setVisible(true);
+    }
+}
