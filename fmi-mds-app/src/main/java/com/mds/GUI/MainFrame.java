aed1d27c32be82e51b734b8de01f4e9cba093e02
kellasone <a_mihai_lo@yahoo.ro>
2019-05-28 12:37:30 +0300

    Added a new panel, menuPanel

--- fmi-mds-app/src/main/java/com/mds/GUI/MainFrame.java
+++ fmi-mds-app/src/main/java/com/mds/GUI/MainFrame.java
@@ -5,11 +5,14 @@ import java.awt.*;
 
 public class MainFrame extends JFrame {
     private JTabbedPane tabbedPane1;
+    private JPanel rootPanel;
 
     public MainFrame()
     {
+        add(rootPanel);
         setTitle("Fmi APP");
         setSize(750, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Dimension minDimension = new Dimension(750, 500);
         Dimension maxDimension = new Dimension(1800, 800);
         setMinimumSize(minDimension);
--- fmi-mds-app/src/main/java/com/mds/GUI/Test.java
+++ fmi-mds-app/src/main/java/com/mds/GUI/Test.java
@@ -3,6 +3,7 @@ package com.mds.GUI;
 public class Test {
     public static void main(String[] args) {
         MainFrame x = new MainFrame();
+
         x.setVisible(true);
     }
 }
